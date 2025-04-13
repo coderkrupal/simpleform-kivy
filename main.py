13927327d6c5e4ac6030178bfd817e4ecc153fb8
coderkrupall <krupaldoshi01@gmail.com>
2025-04-13 11:29:09 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 from kivy.app import App
-from kivy.uix.label  import Label
+from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 
-class MyGridLayout(GridLayout):
-    def __init__(self,**kwargs):
-        super(MyGridLayout,self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text = "name:"))
-        self.name = TextInput(multiline = False)
-        self.add_widget(self.name)
-        
-        self.add_widget(Label(text = "Branch:"))
-        self.brnach = TextInput(multiline = False)
-        self.add_widget(self.brnach)
-        
-        self.add_widget(Label(text = "En No:"))
-        self.enno = TextInput(multiline = False)
-        self.add_widget(self.enno)
-        
-        self.add_widget(Label(text = "year:"))
-        self.year = TextInput(multiline = False)
-        self.add_widget(self.year)
-        
-        
+
+class MyGridLayout(Widget):
     
+    name = ObjectProperty(None)
+    id = ObjectProperty(None)
+    
+    
+
+    def press(self):
+        name = self.name.text
+        rollno = self.rollno.text
+
+        print(f"{name},{rollno}")
+
+        self.name.text = " "
+        self.rollno.text = " "
+
 class MyApp(App):
     def build(self):
         return MyGridLayout()
-    
+
+
 if __name__ == "__main__":
     MyApp().run()
